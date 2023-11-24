0d1698136f3fccc7092cce93db1bc657f236197d
naho-miyazawa <47652704+naho-miyazawa@users.noreply.github.com>
2023-11-24 12:31:06 +0900

    再提出します

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,20 +1,15 @@
-num = 30;
-
-if ((num % 3) === 0) {
-    console.log('3の倍数です');
-} else {
-    console.log(num);
-}
-
-if ((num % 5) == 0) {
-    console.log('5の倍数です');
-} else {
-    console.log(num);
-}
+num = 19;
 
 if ((num % 3) == 0 && (num % 5) == 0) {
     console.log('3と5の倍数です');
-} else {
+}
+else if ((num % 3) === 0) {
+    console.log('3の倍数です');
+} 
+else if ((num % 5) == 0) {
+    console.log('5の倍数です');
+}  
+ else {
     console.log(num);
 }
 
