12cd81d019b61eb7bb0445329af5e55fdfe8c77b
naho-miyazawa <47652704+naho-miyazawa@users.noreply.github.com>
2023-11-26 11:31:45 +0900

    MDN課題提出します

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const moonLanding = new Date();
+
+const Yaer = (moonLanding.getFullYear());
+
+const monTh = (moonLanding.getMonth()) + 1;
+
+const date = (moonLanding.getDate());
+
+console.log(Yaer + '年', monTh + '月', + date + '日');
